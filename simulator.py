c9e49b366172ec2e34e2adfe04cd0fd8d4f41d0d
Osvaldo Castellanos <ocstllns@gmail.com>
2018-10-12 12:17:29 -0500

    Updated quite a bit. Still need to make more changes though.

---
+++ simulator.py
@@ -0,0 +1,17 @@
+from OS import OS
+from ProcessImage import ProcessImage
+
+class Simulator():
+    def __init__(self):
+        self = self
+
+
+
+
+
+if __name__ == '__main__':
+    operating_system = OS()
+    operating_system.begin()
+
+
+
